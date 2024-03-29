d9ba13c0ab9b84795816b298a2ea3024437730ea
ronit16 <ronitshah350@gmail.com>
2024-03-29 10:01:00 +0530

    update app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 // import logo from './logo.svg';
 // import './App.css';
 // import EventList from "./components/events";
-import EventList from "./components/eventsql";
+import EventList from "./components/Event/eventsql";
+import GenNotice from "./components/GenNotice";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Tech Conference Schedule</h1>
+        <GenNotice/>
         <EventList />
       </header>
     </div>
@@ -15,3 +16,43 @@ function App() {
 }
 
 export default App;
+
+
+// import "./App.css";
+// import Profile from "./components/Profile/Profile";
+// import Login from "./components/Login/Login";
+// import Register from "./components/Register/Register";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import { useState } from "react";
+
+// function App() {
+//   const [userstate, setUserState] = useState({});
+//   return (
+//     <div className="App">
+//       <Router>
+//         <Routes>
+//           <Route
+//             path="/"
+//             element={
+//               userstate && userstate._id ? (
+//                 <Profile
+//                   setUserState={setUserState}
+//                   username={userstate.fname}
+//                 />
+//               ) : (
+//                 <Login setUserState={setUserState} />
+//               )
+//             }
+//           ></Route>
+//           <Route
+//             path="/login"
+//             element={<Login setUserState={setUserState} />}
+//           ></Route>
+//           <Route path="/signup" element={<Register />}></Route>
+//         </Routes>
+//       </Router>
+//     </div>
+//   );
+// }
+
+// export default App;
