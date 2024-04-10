d3ac0f0bc4e4f0d46a4d691fa0c760be95e30e7b
ronit16 <ronitshah350@gmail.com>
2024-04-10 08:56:58 +0530

    App updated

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import Testt from "./components/mainpage/test1";
 import Login from "./components/Login/Login";
 import Register from "./components/Register/Register";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useNavigate } from "react-router-dom";
 import { useState } from "react";
 import Acadlist from "./components/Academics/Acadsql";
 import GenNotice from "./components/GenNotice/GenNotice";
@@ -20,7 +20,7 @@ function App() {
                 <Login setUserState={setUserState} />)} ></Route>
           <Route path="/login" element={<Login setUserState={setUserState} />}></Route>
           <Route path="/signup" element={<Register />}></Route>
-          <Route path="/profile" element={<Testt/>}></Route>
+          <Route path="/profile" element={<Testt userstate={userstate}/>}></Route>
           <Route path="/academics" element={<Acadlist/>}></Route>
           <Route path="/notice" element= {<GenNotice/>}></Route>
           <Route path="/achievements" element={<AchiList/>}></Route>
